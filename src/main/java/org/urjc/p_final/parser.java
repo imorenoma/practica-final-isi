c113312bb80a4d54a6adef093644c7c7e5c8af74
TomasGalindo <tgalindoencinas@yahoo.com>
2018-05-31 13:01:05 +0200

    Add File with parser funcitons

---
+++ src/main/java/org/urjc/p_final/parser.java
@@ -0,0 +1,43 @@
+package org.urjc.p_final;
+
+public class parser {
+	
+	public static String parserFilm(String linea){
+		//Quita hasta el ultimo espacio
+		int indice;
+		
+		indice = linea.lastIndexOf('(');
+		//System.out.println("Indice = " + indice);
+		linea = linea.substring(0, indice-1);
+		return linea;
+		
+	}
+	
+	public static String parserFecha(String linea){
+		String fecha;
+		int indice;
+		
+		indice = linea.lastIndexOf('(');
+		fecha = linea.substring(indice+1, linea.length()-1);
+		//System.out.println("fecha =" + fecha);
+		return fecha;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "$300 y tickets (2002)";
+		//Pruebas parserFilm
+		/*
+		String fin = parserFilm(s); 
+		System.out.println(fin);
+		System.out.println(fin.length());
+		System.out.println(fin.charAt(13));
+		*/
+		//Pruebas ParserFecha
+		String fecha = parserFecha(s);
+		System.out.println(fecha);
+
+	}
+
+}
