91aa3556b18bcc5cf1df83604c4137244f48b890
Christian Bermejo Guerrero <cbermejog7@gmail.com>
2018-06-13 22:57:26 +0200

    Añadiendo Test PathFinder

--- pruebasChristian/TestFinder.java
+++ pruebasChristian/TestFinder.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+// METODOS PATHFINDER
+
+// boolean hasPathTo(String v) -- > is v reachable from the source s?
+// int distanceTo(String v) -- > return the length of the shortest path from v to s
+// Iterable<String> pathTo(String v) -- > return the shortest path from v to s as an Iterable
+
+
 public class TestPathFinder {
 
   Graph graph;
@@ -12,17 +19,49 @@ public class TestPathFinder {
   @Before
 	public void setUp() {
 		graph = new Graph("resources/routes.txt", " ");
-		finder = new PathFinder(g, "ATL");
+		finder = new PathFinder(graph, "JFK");
 	}
 
   @Test (expected = NullPointerException.class)
   public void testNullString () {
-    finder = new PathFinder(g, null);
+    finder = new PathFinder(graph, null);
   }
 
   @Test (expected = NullPointerException.class)
   public void testNullGraph () {
-    pf = new PathFinder(null, "ATL");
+    finder = new PathFinder(null, "JFK");
+  }
+
+  @Test
+  public void testUnknownString() {
+    assertEquals("Return the length og the shortest path from unknown vertex.", finder.distanceTo("GFK"), Integer.MAX_VALUE);
+    assertEquals("Return the shortest path from unknown vertex.", finder.pathTo("GFK").toString(), "");
+  }
+
+  @Test
+  public void testRightPath () {
+    //finder = new PathFinder(graph, "JFK");
+    assertEquals("Should be fine.", finder.pathTo("DFW").toString(), "JFK ORD DFW");
+  }
+
+  @Test
+  public void testWrongPath () {
+    //finder = new PathFinder(graph, "JFK");
+    assertEquals("Should be wrong.", finder.pathTo("MCO").toString(), "JFK ATL MCO");
+  }
+
+  @Test
+  public void testHappyPath () {
+    //finder = new PathFinder(graph, "JFK");
+    assertEquals("Should be OK.", finder.pathTo("MCO").toString(), "JFK MCO");
+    assertEquals("Should be OK.", finder.distanceTo("MCO"), 1);
+  }
+
+  @Test
+  public void testHappyWrongPath () {
+    //finder = new PathFinder(graph, "JFK");
+    assertEquals("Should be KO.", finder.pathTo("MCO").toString(), "JFK ATL MCO");
+    assertEquals("Should be KO.", finder.distanceTo("MCO"), 2);
   }
 
 
